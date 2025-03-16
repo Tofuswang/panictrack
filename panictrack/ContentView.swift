5c0942b886a4007c0225758c97923a281fc9acc5
Tofuswang <terry.f.wang@gmail.com>
2025-03-16 22:23:03 +0800

    edited

--- panictrack/ContentView.swift
+++ panictrack/ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -75,13 +74,6 @@ struct ContentView: View {
         }
     }
 }
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
